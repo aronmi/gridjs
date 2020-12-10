3ee8f86c7b1bab23856a487dae8e66e2e27fb2cf
employee <consolsys@employees-MacBook-Pro.local>
2020-12-11 00:08:23 +0800

    adds stylingCell bool prop to TD component

--- packages/gridjs/tests/view/table/td.test.tsx
+++ packages/gridjs/tests/view/table/td.test.tsx
@@ -3,6 +3,8 @@ import { createContext, h } from 'preact';
 import { TD } from '../../../src/view/table/td';
 import Cell from '../../../src/cell';
 import { Config } from '../../../src/config';
+import { EventEmitter } from '../../../src/util/eventEmitter';
+import { TableEvents } from '../../../src/view/table/events';
 
 describe('TD component', () => {
   let config: Config;
@@ -19,5 +21,22 @@ describe('TD component', () => {
       </configContext.Provider>,
     );
     expect(td.html()).toMatchSnapshot();
+  });
+
+  it('should emit cellClick', async () => {
+    config.eventEmitter = new EventEmitter<TableEvents>();
+    const onClick = jest.fn();
+
+    const cells = mount(
+      <configContext.Provider value={config}>
+        <TD cell={new Cell('boo')} />
+      </configContext.Provider>,
+    ).find('td');
+
+    config.eventEmitter.on('cellClick', onClick)
+    cells.map(td => td.simulate('click'));
+
+    expect(cells.length).toEqual(1);
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
